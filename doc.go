63452faf18de420686e48fa0aeb7ebb9d512a16b
Stas Turlo <stanislav.turlo@rightscale.com>
2016-04-28 11:37:18 +0300

    Update demo app deps

--- doc.go
+++ doc.go
@@ -9,26 +9,26 @@ Pushing Time Series Data
 
 The following example shows a data ingestion request:
 
-api := api.Ingest("wss://ingest_url", accessToken, predixZoneId)
-m := api.IngestMessage()
-t, _ := m.AddTag("test_tag")
-t.AddDatapoint(measurement.Int(123), dataquality.Good)
-t.SetAttribute("key", "value")
-m.Send()
+	api := api.Ingest("wss://ingest_url", accessToken, predixZoneId)
+	m := api.IngestMessage()
+	t, _ := m.AddTag("test_tag")
+	t.AddDatapoint(measurement.Int(123), dataquality.Good)
+	t.SetAttribute("key", "value")
+	m.Send()
 
 Querying Time Series Data
 
 You can use the time series API to list tags and attributes as well as to query data points. You can query data points using a start time, end time, tag names, time range, measurement, and attributes. The following example shows a data query with a limit of 1000 set for the number of data points to return in the query result:
 
-api := api.Query("https://query_url", accessToken, predixZoneId)
-q := api.Query().Interval(query.R(5).MinutesAgo(), query.AbsTime(time.Now()))
-q.Tags(query.Tag("ALT_SENSOR", "TEMP_SENSOR").
-  Limit(1000).
-  Aggregation("avg", aggregation.Interval(1).Hour()).
-  Filter(tag.Attributes(tag.Attr("host","<host>"), tag.Attr("type", "<type>"))).
-  Filter(measurement  .Ge(measurement.Float(23.1))).
-  Filter(dataquality.Qualities(dataquality.Bad, dataquality.Good)).
-  GroupByAttributes("attributename1", "attributename2"))
+	api := api.Query("https://query_url", accessToken, predixZoneId)
+	q := api.Query().Interval(query.R(5).MinutesAgo(), query.AbsTime(time.Now()))
+	q.Tags(query.Tag("ALT_SENSOR", "TEMP_SENSOR").
+		Limit(1000).
+		Aggregation("avg", aggregation.Interval(1).Hour()).
+		Filter(tag.Attributes(tag.Attr("host","<host>"), tag.Attr("type", "<type>"))).
+		Filter(measurement  .Ge(measurement.Float(23.1))).
+		Filter(dataquality.Qualities(dataquality.Bad, dataquality.Good)).
+		GroupByAttributes("attributename1", "attributename2"))
 
 Query Properties
 
@@ -38,12 +38,12 @@ Using Relative Start and End Times in a Query
 
 This example shows a query using a relative start time and an absolute end time.
 
-q := api.Query().Interval(query.R(1).YearsAgo(), query.AbsTime(time.Now()))
+	q := api.Query().Interval(query.R(1).YearsAgo(), query.AbsTime(time.Now()))
 
 Limiting the Data Points Returned by a Query
 
-q := api.Query().Start(query.R(15).MinutesAgo())
-q.Tags(q.Tag("ALT_SENSOR").Limit(1000))
+	q := api.Query().Start(query.R(15).MinutesAgo())
+	q.Tags(q.Tag("ALT_SENSOR").Limit(1000))
 
 Data points in the query result cannot exceed the maximum limit of 200,000. Narrow your query criteria (for example, time window) to return a fewer number of data points.
 
@@ -51,8 +51,8 @@ Specifying the Order of Data Points Returned by a Query
 
 The default order for query results is ascending, and it is ordered by timestamp. You can use the `InvertOrder` method to invert the order in which the data points returned.
 
-q := api.Query().Start(query.R(15).MinutesAgo())
-q.Tags(q.Tag("ALT_SENSOR").InvertOrder())
+	q := api.Query().Start(query.R(15).MinutesAgo())
+	q.Tags(q.Tag("ALT_SENSOR").InvertOrder())
 
 Querying for the Latest Data Point
 
@@ -62,7 +62,7 @@ Querying for the Latest Data Point
 
 * If you do not define a time window, the query retrieves the latest data points from the current time (now). The following shows an example of a query for the latest data point with no time window defined:
 
-q := api.Query()
-q.LatestDatapoints(query.Tag("ALT_SENSOR").Filter(measurement.Le(10)))
+	q := api.Query()
+	q.LatestDatapoints(query.Tag("ALT_SENSOR").Filter(measurement.Le(10)))
 */
 package go_predix_timeseries
--- examples/go-predix-timeseries-demo/Godeps/_workspace/src/github.com/Altoros/go-predix-timeseries/aggregation/datapoints.go
+++
@@ -1,5 +0,0 @@
-package aggregation
-
-func Datapoints(datapoints int) Parameter {
-	return Param("sampling", map[string]interface{}{"datapoints": datapoints})
-}
